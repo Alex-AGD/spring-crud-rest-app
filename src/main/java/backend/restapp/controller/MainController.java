b8490fd3e1847f4e1ec763ed1e343c85c17ac007
Aliaksei Dauhapolau <aliaksei.dauhapolau@gmail.com>
2021-03-17 18:08:44 +0300

    Add Main controller for description

---
+++ src/main/java/backend/restapp/controller/MainController.java
@@ -0,0 +1,15 @@
+package backend.restapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String main() {
+        return "main";
+    }
+}
+

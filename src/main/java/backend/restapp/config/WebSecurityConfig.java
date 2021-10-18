aa7065e164a2ff0024d1cc6465abb3dd81316493
Dolgopolov.AG <]9:)gqSU8G3y>
2021-10-18 09:17:24 +0300

    Add Security и oAuth2

--- src/main/java/backend/restapp/config/WebSecurityConfig.java
+++ src/main/java/backend/restapp/config/WebSecurityConfig.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth
 import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 import java.time.LocalDateTime;
 
@@ -18,12 +20,14 @@ import java.time.LocalDateTime;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-
-                .mvcMatchers("/").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .csrf().disable();
+        http.authorizeRequests(a -> a
+                        .antMatchers("/", "/login**", "/error", "/js/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .csrf().disable()
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
     }
 
     @Bean
